d1f411038e5f44d77cb0bf0683ada49c3046dc02
B_Yoda04 <gerardmatos1990@gmail.com>
2022-09-18 01:37:45 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Text from '../components/Text'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='homeCtn'>
+        <Text />
+      </div>
+    </div>
+  )
+}
+
+export default Home
