7c0aabcc189f957408061df377fd70051cc24493
B_Yoda04 <gerardmatos1990@gmail.com>
2022-09-18 13:38:56 -0500

    18/09/2022

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css';
 import './styles/Home.css';
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
+import Proob from './pages/Proob';
 
 function App() {
 
   return (
-    <HashRouter>
+    // <HashRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //   </Routes>
+    // </HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/proob' element={<Proob />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Proob.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Proob() {
+  return (
+    <div>Prueba browser router</div>
+  )
+}
+
+export default Proob
